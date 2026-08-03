3477ca0aa0b638b2efaa50ea8b43afd9ddf8bc22
epshiba122-cpu <epshiba122@gmail.com>
2026-08-03 21:04:19 +0530

    Render port fix

--- app.py
+++ app.py
@@ -248,4 +248,4 @@ def download_pdf():
     return send_file(temp_file.name, as_attachment=True, download_name="lecture_notes.pdf")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+   app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
